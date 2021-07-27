76d42aaeedc08c0862d6742000175b9c5c7b1f54
kimsternator <sskim@ucsd.edu>
2021-07-26 20:28:24 -0700

    Add Links

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,23 +7,23 @@ export default function Projects() {
                 <h2>Projects</h2>
             </div>
             <div className="squares">
-                <div className="square">
+                <div className="square" onClick={() => {window.open("https://github.com/kimsternator")}}>
                     <img src="icons/c++.png" alt="C++" />
                     <h2>Relational Database</h2>
                 </div>
-                <div className="square">
+                <div className="square" onClick={() => {window.open("https://github.com/kimsternator/Anime-Recommender---Visualizer")}}>
                     <img src="icons/python.png" alt="Python" />
                     <h2>Anime Recommender System</h2>
                 </div>
-                <div className="square">
+                <div className="square" onClick={() => {window.open("https://github.com/kimsternator/ServiceUp")}}>
                     <img src="icons/python.png" alt="Python" />
                     <h2>Community Posting Board MVP</h2>
                 </div>
-                <div className="square">
-                    <img src="icons/react.png" href="https://stephenskim.codes/sorting-visualizer/" alt="React" />
+                <div className="square" onClick={() => {window.open("https://stephenskim.codes/sorting-visualizer/")}}>
+                    <img src="icons/react.png" alt="React" />
                     <h2>Sorting Algorithm Visualizer</h2>
                 </div>
-                <div className="square">
+                <div className="square" onClick={() => {window.open("https://github.com/kimsternator")}}>
                     <img src="icons/react.png" alt="React" />
                     <h2>Path Finding Visualizer</h2>
                 </div>
